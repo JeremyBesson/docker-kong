ae3aa5111fb73c0e73ca4a67e33723405954804a
Jérémy Besson <jebes@smile.fr>
2017-09-22 16:11:30 +0200

    kong with postgresql cli

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,9 +7,13 @@ export KONG_NGINX_DAEMON="off"
 # Setting default prefix (override any existing variable)
 export KONG_PREFIX="/usr/local/kong"
 
-kong migrations up -c /usr/local/kong/nginx.conf 
-
-kong reload
+#if ["psql -h kong-database -U kong -d kong -c \"SELECT 1\"" ]; then
+#
+#    kong migrations up #-c /usr/local/kong/nginx.conf 
+#
+#    kong reload
+#
+#fi
 
 # Prepare Kong prefix
 if [ "$1" = "/usr/local/openresty/nginx/sbin/nginx" ]; then
