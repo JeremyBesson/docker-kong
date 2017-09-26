68f3bafff73aa3a8812d57bf224c61c2587ae225
JeremyBesson <jebes@smile.fr>
2017-09-26 13:50:30 +0200

    Revert to previous version

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,24 +1,7 @@
-#!/bin/sh
+#!/usr/local/bin/dumb-init /bin/bash
 set -e
 
 # Disabling nginx daemon mode
 export KONG_NGINX_DAEMON="off"
-
-# Setting default prefix (override any existing variable)
-export KONG_PREFIX="/usr/local/kong"
-
-#if ["psql -h kong-database -U postgres -d kong -c "SELECT 1"" ]; then
-#
-#    kong migrations up #-c /usr/local/kong/nginx.conf 
-#
-#    kong reload
-#
-#fi
-
-# Prepare Kong prefix
-if [ "$1" = "/usr/local/openresty/nginx/sbin/nginx" ]; then
-	kong prepare -p "/usr/local/kong"
-fi
-
 
 exec "$@"
